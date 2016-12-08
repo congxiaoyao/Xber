800bf99ff9892712f5c4a6911e793be5a5a84bb7
Jaycejia <jayce_jia@163.com>
2016-12-08 23:32:34 +0800

    虫子你个逗比

--- src/main/java/com/congxiaoyao/controller/TestController.java
+++ src/main/java/com/congxiaoyao/controller/TestController.java
@@ -13,6 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping(value = "test", method = RequestMethod.POST)
     public TestPO echo(@RequestBody TestPO testPO) {
-        return testPO;
+        return testPO;//echo
     }
 }
