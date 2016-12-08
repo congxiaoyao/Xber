1c1d60a49d1a348a1cf8530fbe3d93411dc5931a
Jaycejia <jayce_jia@163.com>
2016-12-08 23:27:46 +0800

    虫子你个逗比

---
+++ src/main/java/com/congxiaoyao/pojo/TestPO.java
@@ -0,0 +1,33 @@
+package com.congxiaoyao.pojo;
+
+/**
+ * Created by Jaycejia on 2016/12/8.
+ */
+public class TestPO {
+    private String key;
+    private String value;
+
+    public TestPO() {
+    }
+
+    public TestPO(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
