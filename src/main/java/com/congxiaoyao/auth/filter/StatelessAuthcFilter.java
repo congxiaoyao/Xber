04a2c4ad749892dc67320a9db95b7d3ca24c1dea
Jaycejia <jayce_jia@163.com>
2017-02-12 19:04:41 +0800

    实现WEBSOCKET认证

--- src/main/java/com/congxiaoyao/auth/filter/StatelessAuthcFilter.java
+++ src/main/java/com/congxiaoyao/auth/filter/StatelessAuthcFilter.java
@@ -6,6 +6,7 @@ import com.congxiaoyao.auth.exception.NoAuthenticatedInfoException;
 import com.congxiaoyao.auth.pojo.StatelessSession;
 import com.congxiaoyao.auth.service.def.SessionService;
 import com.congxiaoyao.auth.token.StatelessToken;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.AccessControlFilter;
 import org.slf4j.Logger;
@@ -26,45 +27,48 @@ public class StatelessAuthcFilter extends AccessControlFilter {
     private Logger logger = LoggerFactory.getLogger(StatelessAuthcFilter.class);
     @Autowired
     private SessionService sessionService;
+
     @Override
     protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
         return false;
     }
+
     @Override
     protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
         HttpServletRequest httpServletRequest = (HttpServletRequest) request;
         //1、根据客户端认证信息生成无状态Token
         String requestMessage = httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION);
-        StatelessToken token = new StatelessToken();
-        if (requestMessage == null || "".equals(requestMessage)) {
-            onAuthcFail(request, new NoAuthenticatedInfoException()); //无认证，登录失败
-            return true;
-        } else if (!requestMessage.matches("^Basic \\S+:\\S+$")) {
-            onAuthcFail(request, new InvalidTokenException());
-        } else {
-            String clientId = requestMessage.replace("Basic ", "").split(":")[0];
-            String tokenString = requestMessage.replace("Basic ", "").split(":")[1];
-            token.setClientId(clientId);
-            token.setAccessTokenString(tokenString);
-        }
-        //2、委托给Realm进行认证
         try {
-            Subject subject = getSubject(request, response);
-            subject.login(token);
-            StatelessSession session = sessionService.getSessionByClientId((String) subject.getPrincipal());
-            sessionService.isExpired(session);
-            sessionService.refresh(session);
-            request.setAttribute("userId",session.getUserId());
+            checkToken(requestMessage);
         } catch (Exception e) {
-            onAuthcFail(request, e); //3、认证失败
+            onAuthcFail(request, e);
+        } finally {
             return true;
         }
-        return true;
     }
-    private void onAuthcFail(ServletRequest request, Exception exp) throws IOException {
+
+    private void onAuthcFail(ServletRequest request, Exception exp) {
         if (logger.isTraceEnabled()) {
             logger.trace(exp.getMessage(), exp);
         }
         request.setAttribute("shiroLoginFailure", exp);
     }
+
+    public static void checkToken(String requestMessage) {
+        StatelessToken token = new StatelessToken();
+        if (requestMessage == null || "".equals(requestMessage)) {
+            throw new NoAuthenticatedInfoException();//无认证，登录失败
+        } else if (!requestMessage.matches("^Basic \\S+:\\S+$")) {
+            throw new InvalidTokenException();
+        } else {
+            String clientId = requestMessage.replace("Basic ", "").split(":")[0];
+            String tokenString = requestMessage.replace("Basic ", "").split(":")[1];
+            token.setClientId(clientId);
+            token.setAccessTokenString(tokenString);
+            //委托认证
+            Subject subject = SecurityUtils.getSubject();
+            subject.login(token);
+        }
+    }
+
 }
--- src/main/java/com/congxiaoyao/websocket/bean/WebSocketHandshakeInterceptor.java
+++ src/main/java/com/congxiaoyao/websocket/bean/WebSocketHandshakeInterceptor.java
@@ -1,5 +1,6 @@
 package com.congxiaoyao.websocket.bean;
 
+import com.congxiaoyao.auth.filter.StatelessAuthcFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.server.ServerHttpRequest;
@@ -7,17 +8,28 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
+import java.util.List;
 import java.util.Map;
 
 /**
  * Created by Jaycejia on 2016/12/11.
  */
-public class WebSocketHandshakeInterceptor implements HandshakeInterceptor{
+public class WebSocketHandshakeInterceptor implements HandshakeInterceptor {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
-        logger.info(request.getRemoteAddress() + " connecting;headers" + request.getHeaders());
-        return true;
+        logger.debug(request.getRemoteAddress() + " connecting;headers" + request.getHeaders());
+        List<String> tokens = request.getHeaders().get("Authorization");
+        if (tokens.size() != 1) {
+            return false;
+        }
+        try {
+            StatelessAuthcFilter.checkToken(tokens.get(1));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
